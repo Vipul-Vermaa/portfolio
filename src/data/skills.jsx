a518812055c0b3342b988448c5d3481fafae11db
Vipul Verma <vipulvermalm8@gmail.com>
2024-07-12 00:21:11 +0530

    added firebase

--- src/data/skills.jsx
+++ src/data/skills.jsx
@@ -1,14 +1,15 @@
 export const skills = [
-    {name:'HTML',},
-    {name:'CSS',},
-    {name:'JavaScript',},
-    {name:'React',},
-    {name:'MongoDB',},
-    {name:'Node.js',},
-    {name:'Express.js',},
-    {name:'AWS',},
-    {name:'Docker',},
-    {name:'Full Stack',},
+    {name:'HTML'},
+    {name:'CSS'},
+    {name:'JavaScript'},
+    {name:'React'},
+    {name:'MongoDB'},
+    {name:'Node.js'},
+    {name:'Express.js'},
+    {name:'AWS'},
+    {name:'Docker'},
+    {name:'Full Stack'},
+    {name:'Firebase'},
 ];
         
     
